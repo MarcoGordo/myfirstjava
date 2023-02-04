f4e0469f8e9bff52844a868d2d509e9804bc21b6
Marco-PulidoS <36222345+MarcoGordo@users.noreply.github.com>
2023-02-04 03:39:48 -0500

    Add files via upload

---
+++ myproyecto/src/com/primerEjercicio/claseEjercicioUno.java
@@ -0,0 +1,28 @@
+package com.primerEjercicio;
+
+public class claseEjercicioUno {
+
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        float numero2 = 2.2f;
+        double numero3 = 3.33d;
+        long numero4 = 4;
+        boolean verdad = true;
+        boolean mentira = false;
+        char letra = 'a';
+        String nombre = "Mag";
+        Integer num = null;
+        Long ll= 2L;
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(verdad);
+        System.out.println(mentira);
+        System.out.println(letra);
+        System.out.println(nombre);
+        System.out.println(num);
+        System.out.println(ll);
+    }
+}
